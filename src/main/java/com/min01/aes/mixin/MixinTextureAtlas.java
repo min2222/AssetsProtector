a34c2446ae86e0b42a7c054ac4e82147d9120758
min <goyoumin2@gmail.com>
2024-06-08 11:18:10 +0900

    1.19.2

--- src/main/java/com/min01/aes/mixin/MixinTextureAtlas.java
+++ src/main/java/com/min01/aes/mixin/MixinTextureAtlas.java
@@ -18,14 +18,16 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.min01.aes.AESUtil;
 
-import net.minecraft.client.renderer.texture.SpriteLoader;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.server.packs.resources.Resource;
 
-@Mixin(SpriteLoader.class)
-public class MixinSpriteLoader
+@Mixin(TextureAtlas.class)
+public class MixinTextureAtlas
 {
-	@Redirect(method = "loadSprite", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/Resource;open()Ljava/io/InputStream;"))
-	private static InputStream loadSprite(Resource instance) throws IOException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException, InvalidKeySpecException
+	//lambda$getBasicSpriteInfos$2
+	//m_174717_
+	@Redirect(method = "lambda$getBasicSpriteInfos$2", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/Resource;open()Ljava/io/InputStream;"))
+	private InputStream getBasicSpriteInfos(Resource instance) throws IOException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException, InvalidKeySpecException
 	{
         InputStream inputStream = ImageIO.read(instance.open()) != null ? instance.open() : AESUtil.decryptTexture(instance.open().readAllBytes());
 		return inputStream;
