8f428d8259b9614a0795635f990705cf0492d445
min <goyoumin2@gmail.com>
2024-07-02 22:53:44 +0900

    fix

--- src/main/java/com/min01/aes/mixin/MixinResource.java
+++ src/main/java/com/min01/aes/mixin/MixinResource.java
@@ -13,32 +13,41 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 import org.apache.commons.codec.binary.Base64;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.min01.aes.AESUtil;
 
+import net.minecraft.server.packs.resources.IoSupplier;
 import net.minecraft.server.packs.resources.Resource;
 
 @Mixin(Resource.class)
 public class MixinResource 
 {
-	@Inject(at = @At("TAIL"), method = "open", cancellable = true)
+	@Shadow
+	@Final
+	private IoSupplier<InputStream> streamSupplier;
+	   
+	@Inject(at = @At("HEAD"), method = "open", cancellable = true)
 	private void open(CallbackInfoReturnable<InputStream> cir) throws IOException
 	{
-		InputStream input = cir.getReturnValue();
-		byte[] array = input.readAllBytes();
-		if(Base64.isBase64(Arrays.copyOfRange(array, array.length - 60, array.length - 16)))
+		byte[] array = this.streamSupplier.get().readAllBytes();
+		if(array.length > 60)
 		{
-			try 
+			if(Base64.isBase64(Arrays.copyOfRange(array, array.length - 60, array.length - 16)))
 			{
-				cir.setReturnValue(AESUtil.decrypt(array));
-			}
-			catch(InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException | NoSuchPaddingException | InvalidKeySpecException | IOException e) 
-			{
-				e.printStackTrace();
+				try 
+				{
+					cir.setReturnValue(AESUtil.decrypt(array));
+				}
+				catch(InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException | NoSuchPaddingException | InvalidKeySpecException | IOException e) 
+				{
+					e.printStackTrace();
+				}
 			}
 		}
 	}
